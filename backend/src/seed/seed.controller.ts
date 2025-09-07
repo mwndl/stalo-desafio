dcd7a3a5e943c2939f9b4d69d3b36dae8c02cd12
“Marcos” <marcos.wiendl@gmail.com>
2025-09-07 19:40:35 -0300

    feat: adiciona prefixo v1 nos controllers

--- backend/src/seed/seed.controller.ts
+++ backend/src/seed/seed.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common';
 import { SeedService } from './seed.service';
 
-@Controller('seed')
+@Controller('v1/seed')
 export class SeedController {
   constructor(private readonly seedService: SeedService) {}
 
